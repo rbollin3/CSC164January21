fc5eaf225973f96d38970851a6b8363cf76347c9
rbollin3 <rbollin3@dtcc.edu>
2016-01-21 15:22:38 -0500

    first released version

--- src/P1/Main.java
+++ src/P1/Main.java
@@ -5,7 +5,7 @@ package P1;
 * CSC 164
 * Renee Bolling
 * January 21 2016
-* Version 0.1
+* Version 1.0
 * */
 
 import java.util.Scanner;
@@ -17,10 +17,11 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Jimmy Johns: ");
         char option;
+        double price = 0.0;
+        String sandwich = " ";
 
         do{
             System.out.println("(H)am");
-            System.out.println("(T)urkey");
             System.out.println("(R)oast Beef");
             System.out.println("(S)alami");
             System.out.println("(T)una");
@@ -33,8 +34,37 @@ public class Main {
             choice = choice.toUpperCase();
             //get the first character of the users input
             option = choice.charAt(0);
+                switch(option){
+                    //ham
+                    case 'H':
+                        price = 5.50;
+                        sandwich = "Ham";
+                        break;
 
-            System.out.println("choice = " + choice + "option = " + option);
+                    //turkey
+                    case 'R':
+                        price = 6.00;
+                        break;
+                    case 'S':
+                        price = 6.00;
+                        break;
+                    case 'T':
+                        price = 6.00;
+                        break;
+                    case 'D':
+                        price = 0.0;
+                        break;
+
+                    default:
+                        price = 0.0;
+                        System.out.println("option " + option + " not available");
+
+                }
+
+            //Give the user the order information
+            if(price != 0)
+            System.out.printf(" %s sandwich cost $%.2f\n ",sandwich, price);
+
 
         }while(option != 'D' );
         System.out.println("Thank you for ordering at Jimmy Johns ");
